eda2a73f5e5116fee87715cea652fc1138f66992
ykadir42 <ykadir42@gmail.com>
2017-12-06 21:48:01 -0500

    done fibonacci and gcd

---
+++ funky_town.js
@@ -0,0 +1,21 @@
+var fibonacci = function(n) {
+	if (n < 2) {
+		return n;
+	} else {
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+};
+
+var gcd = function(a, b) {
+	var ans;
+	for (var i = 1; i < Math.min(a + 1, b); i++) {
+		if (a % i == 0 && b % i == 0) {
+			ans = i;
+		}
+	}
+	return ans;
+};
+
+var randomStudent = function(){
+
+};
